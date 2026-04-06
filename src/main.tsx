5425aa61ae08493e6816f6a8f9e175b3931d9947
mike clinton <mikeclinton508@gmail.com>
2026-04-06 14:47:36 +0100

    making the all the wallet show at a single click of the button

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,3 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App'
-// import { WalletProvider } from './WalletSetup'
-// import './index.css'
-
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode>
-//     <WalletProvider>
-//       <App />
-//     </WalletProvider>
-//   </React.StrictMode>
-// )
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
